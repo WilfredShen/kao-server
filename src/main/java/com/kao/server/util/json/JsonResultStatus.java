e8b8eebd489f934e09f743def1f180a290c2c45c
QHR0605 <1348357847@qq.com>
2020-11-16 22:38:20 +0800

    添加反馈码和组件扫描路径

--- src/main/java/com/kao/server/util/json/JsonResultStatus.java
+++ src/main/java/com/kao/server/util/json/JsonResultStatus.java
@@ -5,4 +5,18 @@ package com.kao.server.util.json;
  */
 public class JsonResultStatus {
     static public Integer SUCCESS = 200;
+    static public Integer USERNAMEWRONG = 400;
+    static public Integer USERNAMEISNULL = 403;
+    static public Integer PASSWORDWRONG = 406;
+    static public Integer PASSWORDISNULL = 409;
+    static public Integer PHONENUMBERISNULL = 412;
+    static public Integer PHONENUMBERISEXITED = 413;
+    static public Integer PHONENUMBERWRONG = 415;
+    static public Integer VERIFICATIONCODEISNULL = 418;
+    static public Integer VERIFICATIONCODEWRONG = 421;
+    static public Integer VERIFICATIONCODEGETFAILED = 423;
+    static public Integer USERNAMEISEXITED = 424;
+    static public Integer PASSWORDISNOTTHESAME = 427;
+    static public Integer UPDATEPASSWORDFAILED = 430;
+
 }
