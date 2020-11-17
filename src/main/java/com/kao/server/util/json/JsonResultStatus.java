16f318a9db7815d632ff4138640800b555e31b32
QHR0605 <1348357847@qq.com>
2020-11-17 16:15:18 +0800

    增加参数格式错误码404、越权访问错误码440

--- src/main/java/com/kao/server/util/json/JsonResultStatus.java
+++ src/main/java/com/kao/server/util/json/JsonResultStatus.java
@@ -8,6 +8,7 @@ public class JsonResultStatus {
     static public Integer UNKOWN_ERROR = 400;
     static public Integer USERNAME_WRONG = 401;
     static public Integer USERNAME_ISNULL = 403;
+    static public Integer BAD_REQUEST = 404;
     static public Integer PASSWORD_WRONG = 406;
     static public Integer PASSWORD_ISNULL = 409;
     static public Integer PHONENUMBER_ISNULL = 412;
@@ -19,5 +20,6 @@ public class JsonResultStatus {
     static public Integer USERNAME_ISEXITED = 424;
     static public Integer PASSWORD_ISNOT_THESAME = 427;
     static public Integer UPDATE_PASSWORD_FAILED = 430;
+    static public Integer UNAUTHORIZED_USER = 440;
 
 }
