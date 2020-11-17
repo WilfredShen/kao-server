bfcc4c0ce497944ec274fffa8af6445591bd3b95
QHR0605 <1348357847@qq.com>
2020-11-17 16:15:50 +0800

    优化了return语句

--- src/main/java/com/kao/server/controller/LoginController.java
+++ src/main/java/com/kao/server/controller/LoginController.java
@@ -7,14 +7,17 @@ import com.kao.server.service.impls.SmsServiceImpl;
 import com.kao.server.util.json.JsonResult;
 import com.kao.server.util.json.JsonResultStatus;
 import com.kao.server.util.json.ResultFactory;
+import com.kao.server.util.login.IsLogined;
 import com.kao.server.util.login.SaltGenerator;
 import com.kao.server.util.login.UidGenerator;
+import com.kao.server.util.token.TokenGenerator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+import java.sql.Timestamp;
 
 @Controller
 @CrossOrigin
@@ -34,22 +37,28 @@ public class LoginController {
         String username = jsonObject.getString("username");
         String password = jsonObject.getString("password");
         if (username == null) {
-            return ResultFactory.buildFailJsonResult(JsonResultStatus.USERNAMEISNULL, "用户名不能为空");
+            return ResultFactory.buildFailJsonResult(JsonResultStatus.USERNAME_ISNULL, "用户名不能为空");
         }
         if (password == null) {
-            return ResultFactory.buildFailJsonResult(JsonResultStatus.PASSWORDISNULL, "密码不能为空");
+            return ResultFactory.buildFailJsonResult(JsonResultStatus.PASSWORD_ISNULL, "密码不能为空");
         }
+        JsonResult jsonResult = ResultFactory.buildJsonResult(null,null,null);
         User user = loginService.findUserByUsername(username);
-
         if (user != null && user.getUsername().equals(username)) {
             if (user.getPassword().equals(password)) {
-                return ResultFactory.buildJsonResult(JsonResultStatus.SUCCESS, null, user);
+                String token = TokenGenerator.generateToken(user.getUsername(), String.valueOf(user.getUid()));
+                jsonResult.setStatus(JsonResultStatus.SUCCESS);
+                jsonResult.setMessage(null);
+                jsonResult.setData(token);
             } else {
-                return ResultFactory.buildFailJsonResult(JsonResultStatus.PASSWORDWRONG, "密码错误");
+                jsonResult.setStatus(JsonResultStatus.PASSWORD_WRONG);
+                jsonResult.setMessage("密码错误");
             }
         } else {
-            return ResultFactory.buildFailJsonResult(JsonResultStatus.USERNAMEWRONG, "用户名错误");
+            jsonResult.setStatus(JsonResultStatus.USERNAME_WRONG);
+            jsonResult.setMessage("用户名错误");
         }
+        return jsonResult;
     }
 
     @RequestMapping(value = "/register", method = RequestMethod.POST)
@@ -61,31 +70,45 @@ public class LoginController {
         String phoneNumber = jsonObject.getString("phoneNumber");
         String verificationCode = jsonObject.getString("verificationCode");
         if (username == null) {
-            return ResultFactory.buildFailJsonResult(JsonResultStatus.USERNAMEISNULL, "用户名不能为空");
+            return ResultFactory.buildFailJsonResult(JsonResultStatus.USERNAME_ISNULL, "用户名不能为空");
         }
         if (password == null) {
-            return ResultFactory.buildFailJsonResult(JsonResultStatus.PASSWORDISNULL, "密码不能为空");
+            return ResultFactory.buildFailJsonResult(JsonResultStatus.PASSWORD_ISNULL, "密码不能为空");
         }
         if (phoneNumber == null) {
-            return ResultFactory.buildFailJsonResult(JsonResultStatus.PHONENUMBERISNULL, "手机号不能为空");
+            return ResultFactory.buildFailJsonResult(JsonResultStatus.PHONENUMBER_ISNULL, "手机号不能为空");
         }
         if (verificationCode == null) {
-            return ResultFactory.buildFailJsonResult(JsonResultStatus.VERIFICATIONCODEISNULL, "验证码不能为空");
+            return ResultFactory.buildFailJsonResult(JsonResultStatus.VERIFICATIONCODE_ISNULL, "验证码不能为空");
         }
+        JsonResult jsonResult = ResultFactory.buildJsonResult(null,null,null);
         User user = loginService.findUserNameByUsername(username);
         if (user == null) {
-            if ((loginService.findPhoneNumberByPhoneNumber(phoneNumber)!=null)){
-                return ResultFactory.buildFailJsonResult(JsonResultStatus.PHONENUMBERISEXITED, "该手机号已经存在");
+            if ((loginService.findPhoneNumberByPhoneNumber(phoneNumber) != null)) {
+                jsonResult.setStatus(JsonResultStatus.PHONENUMBER_ISEXITED);
+                jsonResult.setMessage("该手机号已经存在");
+            }else{
+                User newUser = new User();
+                newUser.setUid(UidGenerator.getUID());
+                newUser.setUsername(username);
+                newUser.setPassword(password);
+                newUser.setPhone(phoneNumber);
+                newUser.setSalt(SaltGenerator.getSalt());
+                newUser.setRegisterTime(new Timestamp(System.currentTimeMillis()));
+                loginService.addOne(newUser);
+                jsonResult.setStatus(JsonResultStatus.SUCCESS);
             }
-            loginService.addOne(new User(UidGenerator.getUID(), username, password, SaltGenerator.getSalt(), phoneNumber));
-            return ResultFactory.buildJsonResult(JsonResultStatus.SUCCESS,null,user);
+
         } else {
-            return ResultFactory.buildFailJsonResult(JsonResultStatus.USERNAMEISEXITED, "用户名已经存在");
+            jsonResult.setStatus(JsonResultStatus.PHONENUMBER_ISEXITED);
+            jsonResult.setMessage("用户名已经存在");
         }
+        return jsonResult;
     }
 
     @RequestMapping(value = "/updatepassword", method = RequestMethod.POST)
     @ResponseBody
+    @IsLogined
     public JsonResult updatePassword(@RequestBody JSONObject jsonObject, HttpServletRequest request) {
         String username = jsonObject.getString("username");
         String phoneNumber = jsonObject.getString("phoneNumber");
@@ -93,29 +116,29 @@ public class LoginController {
         String password = jsonObject.getString("password");
         String passwordAgain = jsonObject.getString("passwordAgain");
         if (username == null) {
-            return ResultFactory.buildFailJsonResult(JsonResultStatus.USERNAMEISNULL, "用户名不能为空");
+            return ResultFactory.buildFailJsonResult(JsonResultStatus.USERNAME_ISNULL, "用户名不能为空");
         }
         if (phoneNumber == null) {
-            return ResultFactory.buildFailJsonResult(JsonResultStatus.PHONENUMBERISNULL, "手机号不能为空");
+            return ResultFactory.buildFailJsonResult(JsonResultStatus.PHONENUMBER_ISNULL, "手机号不能为空");
         }
         if (password == null) {
-            return ResultFactory.buildFailJsonResult(JsonResultStatus.PASSWORDISNULL, "新密码不能为空");
+            return ResultFactory.buildFailJsonResult(JsonResultStatus.PASSWORD_ISNULL, "新密码不能为空");
         }
         if (passwordAgain == null) {
-            return ResultFactory.buildFailJsonResult(JsonResultStatus.PASSWORDISNULL, "确认密码不能为空");
+            return ResultFactory.buildFailJsonResult(JsonResultStatus.PASSWORD_ISNULL, "确认密码不能为空");
         }
         if (verificationCode == null) {
-            return ResultFactory.buildFailJsonResult(JsonResultStatus.VERIFICATIONCODEISNULL, "验证码不能为空");
+            return ResultFactory.buildFailJsonResult(JsonResultStatus.VERIFICATIONCODE_ISNULL, "验证码不能为空");
         }
 
         if (!password.equals(passwordAgain)) {
-            return ResultFactory.buildFailJsonResult(JsonResultStatus.PASSWORDISNOTTHESAME, "两次输入密码不一致");
+            return ResultFactory.buildFailJsonResult(JsonResultStatus.PASSWORD_ISNOT_THESAME, "两次输入密码不一致");
         }
         int raws = loginService.updatePassword(username, password);
         if (raws == 1) {
             return ResultFactory.buildFailJsonResult(JsonResultStatus.SUCCESS, null);
         } else {
-            return ResultFactory.buildFailJsonResult(JsonResultStatus.UPDATEPASSWORDFAILED, "修改密码失败,请检查账号是否正确");
+            return ResultFactory.buildFailJsonResult(JsonResultStatus.UPDATE_PASSWORD_FAILED, "修改密码失败,请检查账号是否正确");
         }
     }
 
@@ -123,13 +146,19 @@ public class LoginController {
     @ResponseBody
     public JsonResult getVerificationCode(@RequestBody JSONObject jsonObject, HttpServletRequest request) {
         String phoneNumber = jsonObject.getString("phoneNumber");
+        JsonResult jsonResult = ResultFactory.buildJsonResult(null,null,null);
         if (phoneNumber == null) {
-            return ResultFactory.buildFailJsonResult(JsonResultStatus.PHONENUMBERISNULL, "手机号不能为空");
+            jsonResult.setStatus(JsonResultStatus.PHONENUMBER_ISNULL);
+            jsonResult.setMessage("手机号不能为空");
+            return jsonResult;
         }
         String verificationCode = smsService.getVerificationCode(phoneNumber);
-        if (verificationCode!=null){
-            return ResultFactory.buildFailJsonResult(JsonResultStatus.SUCCESS, null);
+        if (verificationCode != null) {
+            jsonResult.setStatus(JsonResultStatus.SUCCESS);
+        }else {
+            jsonResult.setStatus(JsonResultStatus.VERIFICATIONCODE_GET_FAILED);
+            jsonResult.setMessage("获取验证码失败!请检查手机号是否正确");
         }
-        return ResultFactory.buildFailJsonResult(JsonResultStatus.VERIFICATIONCODEGETFAILED,"获取验证码失败!请检查手机号是否正确");
+        return jsonResult;
     }
 }
