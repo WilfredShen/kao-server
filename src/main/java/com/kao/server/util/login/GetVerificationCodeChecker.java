6a34dda238e8d2f37c5525251eb321e633ef51b1
QHR0605 <1348357847@qq.com>
2020-11-17 16:42:27 +0800

    封装了用户登录的逻辑判断

---
+++ src/main/java/com/kao/server/util/login/GetVerificationCodeChecker.java
@@ -0,0 +1,28 @@
+package com.kao.server.util.login;
+
+import com.kao.server.service.impls.LoginServiceImpl;
+import com.kao.server.service.impls.SmsServiceImpl;
+import com.kao.server.util.json.JsonResult;
+import com.kao.server.util.json.JsonResultStatus;
+import com.kao.server.util.json.ResultFactory;
+
+public class GetVerificationCodeChecker {
+
+    public static JsonResult checkGetVerificationCode(String phoneNumber, LoginServiceImpl loginService, SmsServiceImpl smsService) {
+
+        JsonResult jsonResult = ResultFactory.buildJsonResult(null, null, null);
+        if (phoneNumber == null) {
+            jsonResult.setStatus(JsonResultStatus.PHONENUMBER_ISNULL);
+            jsonResult.setMessage("手机号不能为空");
+            return jsonResult;
+        }
+        String verificationCode = smsService.getVerificationCode(phoneNumber);
+        if (verificationCode != null) {
+            jsonResult.setStatus(JsonResultStatus.SUCCESS);
+        } else {
+            jsonResult.setStatus(JsonResultStatus.VERIFICATIONCODE_GET_FAILED);
+            jsonResult.setMessage("获取验证码失败!请检查手机号是否正确");
+        }
+        return jsonResult;
+    }
+}
