2d640fac10a7f3f0928289aec01793beb438d395
QHR0605 <1348357847@qq.com>
2020-11-17 11:55:23 +0800

    添加未知错误反馈码

--- src/main/java/com/kao/server/util/json/JsonResultStatus.java
+++ src/main/java/com/kao/server/util/json/JsonResultStatus.java
@@ -5,18 +5,19 @@ package com.kao.server.util.json;
  */
 public class JsonResultStatus {
     static public Integer SUCCESS = 200;
-    static public Integer USERNAMEWRONG = 400;
-    static public Integer USERNAMEISNULL = 403;
-    static public Integer PASSWORDWRONG = 406;
-    static public Integer PASSWORDISNULL = 409;
-    static public Integer PHONENUMBERISNULL = 412;
-    static public Integer PHONENUMBERISEXITED = 413;
+    static public Integer UNKOWN_ERROR = 400;
+    static public Integer USERNAME_WRONG = 401;
+    static public Integer USERNAME_ISNULL = 403;
+    static public Integer PASSWORD_WRONG = 406;
+    static public Integer PASSWORD_ISNULL = 409;
+    static public Integer PHONENUMBER_ISNULL = 412;
+    static public Integer PHONENUMBER_ISEXITED = 413;
     static public Integer PHONENUMBERWRONG = 415;
-    static public Integer VERIFICATIONCODEISNULL = 418;
-    static public Integer VERIFICATIONCODEWRONG = 421;
-    static public Integer VERIFICATIONCODEGETFAILED = 423;
-    static public Integer USERNAMEISEXITED = 424;
-    static public Integer PASSWORDISNOTTHESAME = 427;
-    static public Integer UPDATEPASSWORDFAILED = 430;
+    static public Integer VERIFICATIONCODE_ISNULL = 418;
+    static public Integer VERIFICATIONCOD_EWRONG = 421;
+    static public Integer VERIFICATIONCODE_GET_FAILED = 423;
+    static public Integer USERNAME_ISEXITED = 424;
+    static public Integer PASSWORD_ISNOT_THESAME = 427;
+    static public Integer UPDATE_PASSWORD_FAILED = 430;
 
 }
