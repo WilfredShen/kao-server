52a41cff2f81e0c472942239d683eed336e38c4b
QHR0605 <1348357847@qq.com>
2020-11-23 20:56:25 +0800

    Some change

---
+++ src/main/java/com/kao/server/controller/TurtorController.java
@@ -0,0 +1,9 @@
+package com.kao.server.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+public class TurtorController {
+}
---
+++ src/main/java/com/kao/server/dto/UserMessage.java
@@ -0,0 +1,63 @@
+package com.kao.server.dto;
+
+/**
+ * @author 全鸿润
+ */
+public class UserMessage {
+
+    private Integer uid;
+    private String username;
+    private String phone;
+    private String email;
+    private boolean verified;
+
+    public Integer getUid() {
+        return uid;
+    }
+
+    public void setUid(Integer uid) {
+        this.uid = uid;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public boolean isVerified() {
+        return verified;
+    }
+
+    public void setVerified(boolean verified) {
+        this.verified = verified;
+    }
+
+    public String getAccountType() {
+        return accountType;
+    }
+
+    public void setAccountType(String accountType) {
+        this.accountType = accountType;
+    }
+
+    private String accountType;
+}
