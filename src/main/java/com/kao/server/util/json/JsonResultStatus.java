36411364ed8e932bf215906202f79f212e7b545b
WilfredShen <54000440+WilfredShen@users.noreply.github.com>
2020-11-28 12:26:43 +0800

    更新 错误码

--- src/main/java/com/kao/server/util/json/JsonResultStatus.java
+++ src/main/java/com/kao/server/util/json/JsonResultStatus.java
@@ -11,7 +11,21 @@ public class JsonResultStatus {
     static public Integer FAIL = 400;
     static public String FAIL_DESC = "fail";
 
-    static public Integer UNKNOWN_ERROR = 400;
+    static public Integer UNAUTHORIZED = 401;
+    static public String UNAUTHORIZED_DESC = "unauthorized";
+
+    static public Integer ILLEGAL_PARAM = 402;
+    static public String ILLEGAL_PARAM_DESC = "illegal parameters";
+
+    static public Integer NOT_FOUND = 403;
+    static public String NOT_FOUND_DESC = "not found";
+
+    static public Integer UNKNOWN_ERROR = 404;
+    static public String UNKNOWN_ERROR_DESC = "unknown error";
+
+    static public Integer UNCOMPLETED = 1090;
+    static public String UNCOMPLETED_DESC = "未全部完成";
+
     static public Integer USERNAME_WRONG = 401;
     static public Integer USERNAME_ISNULL = 403;
     static public Integer BAD_REQUEST = 404;
@@ -28,12 +42,5 @@ public class JsonResultStatus {
     static public Integer UPDATE_PASSWORD_FAILED = 430;
     static public Integer UNAUTHORIZED_USER = 440;
     static public Integer COLLEGE_ISNULL = 450;
-    static public  Integer GRADUATEDATE_ISNULL = 453;
-
-    static public Integer ILLEGAL_PARAM = 450;
-    static public String ILLEGAL_PARAM_DESC = "非法参数";
-
-
-    static public Integer UNCOMPLETED = 460;
-    static public String UNCOMPLETED_DESC = "未全部完成";
+    static public Integer GRADUATEDATE_ISNULL = 453;
 }
