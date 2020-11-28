3251a82a93de29af1b880c5fb5ba8b372c208e0a
QHR0605 <1348357847@qq.com>
2020-11-28 17:34:51 +0800

    上传评估结果、修改评估结果、查询评估结果

--- src/main/java/com/kao/server/controller/AdminController.java
+++ src/main/java/com/kao/server/controller/AdminController.java
@@ -1,32 +1,87 @@
 package com.kao.server.controller;
 
+import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
-import com.kao.server.service.impl.AdminServiceImpl;
-import com.kao.server.service.impl.LoginServiceImpl;
+import com.kao.server.dto.AdminViewEvaluation;
+import com.kao.server.dto.EvaluationBase;
+import com.kao.server.service.AdminService;
+import com.kao.server.util.intercepter.IsAdmin;
+import com.kao.server.util.intercepter.IsLoggedIn;
 import com.kao.server.util.json.JsonResult;
+import com.kao.server.util.json.JsonResultStatus;
+import com.kao.server.util.json.ResultFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /**
  * @author 全鸿润
  */
-@CrossOrigin
+
 @RestController
-@RequestMapping(value ="/admin")
+@CrossOrigin
+@RequestMapping(value = "/admin")
 public class AdminController {
 
     @Autowired
-    private AdminServiceImpl adminService;
+    AdminService adminService;
 
-
-    @RequestMapping(value="/login",method = RequestMethod.POST)
-    @ResponseBody
-    public JsonResult login(@RequestBody JSONObject adminMsg){
-
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    public JsonResult login(@RequestBody JSONObject adminMsg) {
         String username = adminMsg.getString("username");
         String password = adminMsg.getString("password");
-        System.err.println("Login:"+username+password);
-        return adminService.handleLogin(username,password);
+        System.err.println("Login:" + username + password);
+        return adminService.handleLogin(username, password);
+    }
+
+    @RequestMapping(value = "/get_evaluation", method = RequestMethod.GET)
+    @IsLoggedIn
+    @IsAdmin
+    public JsonResult getEvaluation(@RequestParam() int round) {
+        List<AdminViewEvaluation> data = adminService.findEvaluationByRound(round);
+        return ResultFactory.buildSuccessJsonResult("查找成功", data);
+    }
+
+    @RequestMapping(value = "/upload_evaluation", method = RequestMethod.POST)
+    @IsLoggedIn
+    @IsAdmin
+    public JsonResult uploadEvaluations(@RequestBody JSONObject evaluations) {
+
+        JSONArray res = evaluations.getJSONArray("evaluations");
+        List<EvaluationBase> results = res.toJavaList(EvaluationBase.class);
+        int len = results.size();
+        if (len != adminService.uploadEvaluationResult(results)) {
+            return ResultFactory.buildFailJsonResult(JsonResultStatus.FAIL, JsonResultStatus.FAIL_DESC);
+        }
+        return ResultFactory.buildSuccessJsonResult(JsonResultStatus.SUCCESS_DESC, null);
+    }
+
+    @RequestMapping(value = "/update_evaluation", method = RequestMethod.POST)
+    @IsLoggedIn
+    @IsAdmin
+    public JsonResult updateEvaluation(@RequestBody JSONObject evaluation) {
+
+        String mid = evaluation.getString("mid");
+        String cid = evaluation.getString("cid");
+        String result = evaluation.getString("newResult");
+        String adminId = evaluation.getString("adminId");
+        String round = evaluation.getString("round");
+
+        int raws = adminService.updateEvaluationResult(
+                cid,
+                mid,
+                Integer.parseInt(round),
+                Integer.parseInt(adminId),
+                result
+        );
+
+        if (raws == 1) {
+            return ResultFactory.buildSuccessJsonResult(JsonResultStatus.SUCCESS_DESC, null);
+        }
+
+        return ResultFactory.buildFailJsonResult(JsonResultStatus.FAIL, JsonResultStatus.FAIL_DESC);
+
     }
 
 }
--- src/main/java/com/kao/server/service/AdminService.java
+++ src/main/java/com/kao/server/service/AdminService.java
@@ -1,15 +1,55 @@
 package com.kao.server.service;
 
+import com.kao.server.dto.AdminViewEvaluation;
+import com.kao.server.dto.EvaluationBase;
 import com.kao.server.entity.Admin;
-import com.kao.server.entity.User;
 import com.kao.server.util.json.JsonResult;
+
+import java.util.List;
 
 public interface AdminService {
 
     /**
      * 通过用户名查找管理员
+     *
      * @param username
      * @return Admin
      */
     public Admin findUserByUsername(String username);
+
+    /**
+     * 处理登录逻辑
+     *
+     * @param username
+     * @param password
+     * @return json格式的处理结果
+     */
+    public JsonResult handleLogin(String username, String password);
+
+    /**
+     * 管理员界面的根据轮次获取评估结果
+     *
+     * @param result
+     * @return 评估结果列表
+     */
+    public Integer uploadEvaluationResult(List<EvaluationBase> result);
+
+    /**
+     * @param round
+     * @return
+     */
+    public List<AdminViewEvaluation> findEvaluationByRound(int round);
+
+    /**
+     * 修改当行评估结果
+     *
+     * @param cid
+     * @param mid
+     * @param round
+     * @param adminId
+     * @param result
+     * @return
+     */
+    public Integer updateEvaluationResult(String cid, String mid, int round, int adminId, String result);
+
 }
