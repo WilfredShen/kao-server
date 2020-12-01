f73014db99ae641e87c2901cb1b9d38af19b4f7b
QHR0605 <1348357847@qq.com>
2020-12-01 14:28:53 +0800

    新增dto类

---
+++ src/main/java/com/kao/server/dto/UpdatedStudentMessage.java
@@ -0,0 +1,86 @@
+package com.kao.server.dto;
+
+import java.util.Date;
+
+/**
+ * @author 全鸿润
+ */
+public class UpdatedStudentMessage {
+
+    private String phoneNumber;
+    private String email;
+    private String college;
+    private String major;
+    private String expectedMajor;
+    private Date graduationDate;
+    private Boolean queryable;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getCollege() {
+        return college;
+    }
+
+    public void setCollege(String college) {
+        this.college = college;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getMajor() {
+        return major;
+    }
+
+    public void setMajor(String major) {
+        this.major = major;
+    }
+
+    public String getExpectedMajor() {
+        return expectedMajor;
+    }
+
+    public void setExpectedMajor(String expectedMajor) {
+        this.expectedMajor = expectedMajor;
+    }
+
+    public Date getGraduationDate() {
+        return graduationDate;
+    }
+
+    public void setGraduationDate(Date graduationDate) {
+        this.graduationDate = graduationDate;
+    }
+
+    public Boolean getQueryable() {
+        return queryable;
+    }
+
+    public void setQueryable(Boolean queryable) {
+        this.queryable = queryable;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "UpdatedStudentMessage{" +
+                "phone='" + phoneNumber + '\'' +
+                ", email='" + email + '\'' +
+                ", college='" + college + '\'' +
+                ", major='" + major + '\'' +
+                ", expectedMajor='" + expectedMajor + '\'' +
+                ", graduationDate=" + graduationDate +
+                ", queryable=" + queryable +
+                '}';
+    }
+}
