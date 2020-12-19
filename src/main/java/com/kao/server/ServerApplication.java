c5daae614a74c4081929e02b0c24996990e13781
QHR0605 <1348357847@qq.com>
2020-12-20 01:13:22 +0800

    通过@Bean配置Service层的依赖注入

--- src/main/java/com/kao/server/ServerApplication.java
+++ src/main/java/com/kao/server/ServerApplication.java
@@ -1,8 +1,16 @@
 package com.kao.server;
 
+import com.kao.server.service.AdminService;
+import com.kao.server.service.StudentService;
+import com.kao.server.service.TutorService;
+import com.kao.server.service.impl.AdminServiceImpl;
+import com.kao.server.service.impl.StudentServiceImpl;
+import com.kao.server.service.impl.TutorServiceImpl;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
 
 
 /**
@@ -16,4 +24,21 @@ public class ServerApplication {
         SpringApplication.run(ServerApplication.class, args);
     }
 
+    @Bean
+    @Primary
+    public AdminService getAdminService() {
+        return new AdminServiceImpl();
+    }
+
+    @Bean
+    @Primary
+    public StudentService getStudentService() {
+        return new StudentServiceImpl();
+    }
+
+    @Bean
+    @Primary
+    public TutorService getTutorService() {
+        return new TutorServiceImpl();
+    }
 }
