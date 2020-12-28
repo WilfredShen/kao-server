1c243c2c7fd7923b55fc7a416791454cb1d64e68
QHR0605 <1348357847@qq.com>
2020-12-28 18:29:00 +0800

    redis的键的前缀

---
+++ src/main/java/com/kao/server/util/properties/RedisPrefixProperties.java
@@ -0,0 +1,28 @@
+package com.kao.server.util.properties;
+
+/**
+ * @author 全鸿润
+ */
+public class RedisPrefixProperties {
+
+    public static final String LATEST_NEWS = "latestNews";
+    public static final String NEWS = "news";
+    public static final String LATEST_EVALUATION = "latestEvaluation";
+    public static final String EVALUATIONS = "evaluations";
+    public static final String COLLEGE_ID_AND_NAME = "collegeIdAndName";
+    public static final String DISCIPLINE = "discipline";
+    public static final String MAJOR = "major";
+    public static final String COLLEGE = "college";
+    public static final String TUTOR_WITH_CID = "tutorWithCid";
+    public static final String SUMMER_CAMP = "summerCamp";
+    public static final String EXEMPTION = "exemption";
+    public static final String COLLEGE_LATEST_RANK = "latestCollegeRank";
+    public static final String COLLEGE_RANK = "collegeRank";
+    public static final String ACCEPTANCE_RATE = "acceptanceRate";
+    public static final String ROUND = "round";
+    public static final String RANK = "rank";
+    public static final String STUDENT_ID = "studentId";
+    public static final String FAVOR_MAJOR = "favor_major";
+    public static final String FAVOR_TUTOR = "favor_tutor";
+    public static final String FAVOR_NEWS = "favor_news";
+}
